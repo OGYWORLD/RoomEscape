4807ba7ccd7edc12c81484e26965c0e2bcebfebc
soon <inae.soon@gmail.com>
2024-07-09 04:14:53 +0900

    이번엔 정말루??

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region Inhye
+#endregion
+
+public class Lamp : MonoBehaviour
+{
+    [SerializeField] private Light lampLight;
+    public GameObject password;
+
+    private bool isLight;
+    public bool PreConditionChecked { get; set; } = true;
+
+    private void Start()
+    {
+        lampLight = GetComponentInChildren<Light>();
+        lampLight.gameObject.SetActive(false);
+        password.SetActive(false);
+
+        StartCoroutine(CheckGoalCoroutine());
+    }
+
+    public void LightToggle()
+    {
+        isLight = !isLight;
+        lampLight.gameObject.SetActive(isLight);
+    }
+
+    private bool CheckLightOnCorrectTime()
+    {
+        if(PreConditionChecked && isLight)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    IEnumerator CheckGoalCoroutine()
+    {
+        yield return new WaitUntil(() => CheckLightOnCorrectTime());
+        ShowPassword();
+    }
+
+    private void ShowPassword()
+    {
+        password.SetActive(true);
+    }
+}
