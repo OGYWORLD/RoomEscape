88d80f8c79bff142484a0483c9e4502c49a00352
Haeun99 <cherishu.oxo@gmail.com>
2024-07-09 04:05:02 +0900

    머지테스트

---
+++ Assets/Scripts/PortraitManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region ¿À°¡À»
+#endregion
+
+public class PortraitManager : MonoBehaviour
+{
+    public int Now { get; set; } = 0;
+}
