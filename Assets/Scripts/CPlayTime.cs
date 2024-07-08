f98ed2eeeeba490b40dc81dcdf0d1810dd7a1fb9
OGYWORLD <ogy1004@naver.com>
2024-07-08 19:26:32 +0900

    Fin FirstSecondQuiz

---
+++ Assets/Scripts/CPlayTime.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+#region 오가을
+#endregion
+
+/// <summary>
+/// 플레이 시간을 측정하는 스크립트 입니다.
+/// </summary>
+
+public class CPlayTime : MonoBehaviour
+{
+    private float playTime;
+
+    public TextMeshProUGUI playTimeTMP;
+
+    private bool isEnd = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!isEnd)
+        {
+            isEnd = true;
+
+            float t = Time.time;
+
+            float h = (int)(t / 360);
+            float m = (int)((t % 360) / 60);
+            float s = (int)((t % 360) % 60);
+
+            playTimeTMP.text = "" + h + "H " + m + "M " + s + "S";
+        }
+    }
+
+
+}
